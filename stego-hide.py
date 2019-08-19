29c82d4355afe8fd5291aa61e42e46fccd0fe204
Kevin Frachtenberg Goldsmit <kevin.frachtenberg@mercadolibre.com>
2019-08-19 20:34:31 -0300

    Fixed text length header

--- stego-hide.py
+++ stego-hide.py
@@ -1,19 +1,20 @@
 from PIL import Image
 import random
 
+
 # binary_text is a string like '0b010101'
 def valid_text_length_image_size_and_admissible_bits(binary_text, usable_bits, k):
     if k < 2 or k > 8:
         print("admissible bit is not right")
         return False
-    if usable_bits - (len(binary_text) - 2) - 4 - 32 <= 0:
+    if usable_bits - len(binary_text) - 4 - 32 <= 0:
         print("image is not big enough")
         return False
     return True
 
 
 def text_to_hide_with_padding(text, usable_bits, max_int):
-    padding_size = usable_bits - (len(text) - 2) - 4 - 32
+    padding_size = usable_bits - len(text) - 4 - 32
     result = text
     for n in range(padding_size):
         random_bin = random.randint(0, max_int)
@@ -30,7 +31,9 @@ if __name__ == '__main__':
     input_text = str(input("text to hide"))
     admissible_bits = int(input("admissable bits"))
 
-    text_to_hide = bin(int.from_bytes(input_text.encode(), 'big'))
+    text_to_hide = bin(int.from_bytes(input_text.encode(), 'big'))[2:]
+    if len(text_to_hide) % 2 == 1:
+        text_to_hide = '0' + text_to_hide[2:]
 
     original_image = Image.open(file_name)
     width, height = original_image.size
@@ -45,7 +48,7 @@ if __name__ == '__main__':
     current_text_index_from = 2
     current_text_index_to = current_text_index_from + admissible_bits
 
-    text_length_and_admissible_bits_binary = '{0:032b}'.format(len(text_to_hide)-2) + '{0:04b}'.format(admissible_bits)
+    text_length_and_admissible_bits_binary = '{0:032b}'.format(len(text_to_hide)) + '{0:04b}'.format(admissible_bits)
     header_from = 0
     header_to = 2
     for i in range(width):
@@ -55,7 +58,7 @@ if __name__ == '__main__':
             bin_G = '{0:08b}'.format(g)
             bin_B = '{0:08b}'.format(b)
 
-            if i == 0 and j < 6:
+            if i == 0 and j < 6:    # set headers
                 new_r = int(bin_R[0:6] + text_length_and_admissible_bits_binary[header_from:header_to], 2)
                 header_from += 2
                 header_to += 2
@@ -66,7 +69,7 @@ if __name__ == '__main__':
                 header_from += 2
                 header_to += 2
                 output_image.putpixel((i, j), (new_r, new_g, new_b))
-            else:
+            else:   # set hidden text
                 new_r = int(bin_R[:bits_to_keep] + final_text_binary[current_text_index_from:current_text_index_to], 2)
                 current_text_index_from += admissible_bits
                 current_text_index_to += admissible_bits
