7a896f27bbe5aa49d92856a53066ac85cfffb2ac
Kevin Frachtenberg Goldsmit <kevin.frachtenberg@mercadolibre.com>
2019-08-19 13:01:26 -0300

    Added stego text to image

--- stego-hide.py
+++ stego-hide.py
@@ -1,35 +1,67 @@
-import os
 from PIL import Image
-
+import random
 
 # binary_text is a string like '0b010101'
-def valid_text_length_image_size_and_admissible_bits(binary_text, width, height, k):
+def valid_text_length_image_size_and_admissible_bits(binary_text, usable_bits, k):
     if k < 2 or k > 8:
         print("admissible bit is not right")
         return False
-    total_bites = width * height * 3 * 8
-    if total_bites - (len(binary_text) - 2) - 4 - 32 <= 0:
+    if usable_bits - (len(binary_text) - 2) - 4 - 32 <= 0:
         print("image is not big enough")
         return False
     return True
 
 
-if __name__ == '__main__':
-    file_name = str(input())
-    text = str(input())
-    admissible_bits = int(input())
+def text_to_hide_with_padding(text, usable_bits, max_int):
+    padding_size = usable_bits - (len(text) - 2) - 4 - 32
+    result = text
+    for n in range(padding_size):
+        random_bin = random.randint(0, max_int)
+        binary_representation = '{0:03b}'.format(random_bin)
+        result = result + binary_representation
+    return result
 
-    text_to_hide = bin(int.from_bytes(text.encode(), 'big'))
+
+if __name__ == '__main__':
+    file_name = str(input("file name"))
+    if file_name.find('.bmp') == -1:
+        print("image must be *.bmp")
+        exit()
+    input_text = str(input("text to hide"))
+    admissible_bits = int(input("admissable bits"))
+
+    text_to_hide = bin(int.from_bytes(input_text.encode(), 'big'))
 
     original_image = Image.open(file_name)
     width, height = original_image.size
-    if not valid_text_length_image_size_and_admissible_bits(text_to_hide, width, height, admissible_bits):
+    total_usable_bits = width * height * 3 * admissible_bits
+    if not valid_text_length_image_size_and_admissible_bits(text_to_hide, total_usable_bits, admissible_bits):
         exit()
+
+    final_text_binary = text_to_hide_with_padding(text_to_hide, total_usable_bits, (2**admissible_bits)-1)
     output_image = Image.new('RGB', [width, height], 0)
 
-
+    current_text_index_from = 2
+    bits_to_keep = 8 - admissible_bits
     for i in range(width):
         for j in range(height):
-            r,g,b = original_image.getpixel((i,j))
-            #hide_text_in_pixel(rgba)
-            output_image.putpixel((i,j), [r,g,b])
+            r, g, b = original_image.getpixel((i, j))
+            bin_R = '{0:08b}'.format(r)
+            bin_G = '{0:08b}'.format(g)
+            bin_B = '{0:08b}'.format(b)
+
+
+            current_text_index_to = current_text_index_from + admissible_bits
+            new_r = int(bin_R[0:bits_to_keep] + final_text_binary[current_text_index_from:current_text_index_to], 2)
+            current_text_index_from += admissible_bits
+            current_text_index_to += admissible_bits
+            new_g = int(bin_G[0:bits_to_keep] + final_text_binary[current_text_index_from:current_text_index_to], 2)
+            current_text_index_from += admissible_bits
+            current_text_index_to += admissible_bits
+            new_b = int(bin_B[0:bits_to_keep] + final_text_binary[current_text_index_from:current_text_index_to], 2)
+            current_text_index_from += admissible_bits
+            current_text_index_to += admissible_bits
+            output_image.putpixel((i, j), (new_r, new_g, new_b))
+
+    output_name = file_name.replace('.bmp', '-stego.bmp')
+    output_image.save(output_name)
